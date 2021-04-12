436422d679351001a8d062a3ca56d78c23838ccc
Rytis Sileika <rts.pub@pulegium.eu>
2021-04-12 21:34:08 +0100

    test _request

--- tests/test_client_igclient.py
+++ tests/test_client_igclient.py
@@ -1,4 +1,5 @@
-from pygindex.client import IGClient, IGAPIConfig, IGUserAuth
+import requests
+from pygindex.client import IGClient, IGAPIConfig, IGUserAuth, IGResponse
 
 
 def test_client():
@@ -6,3 +7,30 @@ def test_client():
                       IGAPIConfig("demo"))
     assert isinstance(client._auth, IGUserAuth)
     assert isinstance(client._api, IGAPIConfig)
+
+
+def test_request(monkeypatch):
+
+    class MockResp:
+        def __init__(self):
+            self.status_code = 200
+            self.url = "http://example.com"
+            self.headers = {"h_key": "h_value"}
+
+        def json(self):
+            return {"d_key": "d_value"}
+
+    def req_get(*args, **kwargs):
+        return MockResp()
+
+    monkeypatch.setattr(requests, "get", req_get)
+
+    res = IGClient._request("https://example.com",
+                            "get",
+                            {"d_key": "d_val"},
+                            {"h_key": "h_val"})
+
+    assert isinstance(res, IGResponse)
+    assert res.data == {"d_key": "d_value"}
+    assert res.headers == {"h_key": "h_value"}
+
