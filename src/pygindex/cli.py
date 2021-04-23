1a742f4d587966855fe26c38385f8b4f24c556db
Rytis Sileika <rts.pub@pulegium.eu>
2021-04-23 14:42:23 +0100

    wip cli args

--- src/pygindex/cli.py
+++ src/pygindex/cli.py
@@ -1,8 +1,54 @@
 """Command line interface"""
 
+import argparse
 import sys
+from .utils import Configuration
+
+
+def build_parser():
+    parser = argparse.ArgumentParser(
+        prog="pygi",
+        description="Command line utility to interact with IG Index trading platform",
+    )
+    subparsers = parser.add_subparsers(dest="object", required=True)
+
+    # ** instrument **
+    parser_instrument = subparsers.add_parser("instrument", help="Instruments")
+    subparsers_instrument = parser_instrument.add_subparsers(
+        dest="instrument_command", required=True
+    )
+    # instrument -> search
+    parser_instrument_search = subparsers_instrument.add_parser(
+        "search", help="Search for markets"
+    )
+    parser_instrument_search.add_argument("term", type=str, help="Search term")
+    # instrument -> get
+    parser_instrument_get = subparsers_instrument.add_parser(
+        "get", help="Get instrument details"
+    )
+    parser_instrument_get.add_argument("name", type=str, help="Instrument name")
+
+    # ** positions **
+    parser_positions = subparsers.add_parser("positions", help="Manage open positions")
+    subparsers_positions = parser_positions.add_subparsers(
+        dest="positions_command", required=True
+    )
+    # positions -> get
+    parser_positions_get = subparsers_positions.add_parser(
+        "get", help="Get open positions"
+    )
+    parser_positions_get.add_argument("--all")
+
+    return parser
+
+
+def parse_args(parser):
+    return parser.parse_args()
 
 
 def app():
-    print("Hello!!")
+    args = parse_args(build_parser())
+    conf = Configuration.from_file()
+    print(args)
+    print(conf)
     sys.exit(0)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,4 +6,4 @@ def test_api():
     with pytest.raises(SystemExit) as exc:
         app()
     assert exc.type == SystemExit
-    assert exc.value.code == 0
+    assert exc.value.code == 2
