a689e80937f80d07340068b94612864166bb648a
Rytis Sileika <rts.pub@pulegium.eu>
2021-05-01 17:56:22 +0100

    cli build and dispatch map routing

--- src/pygindex/cli.py
+++ src/pygindex/cli.py
@@ -24,19 +24,62 @@ class GenericCommand(metaclass=CommandMeta):
 
 
 class InstrumentCommand(GenericCommand):
+    """Instruments"""
+
+    cli_name = "instrument"
 
     @cli_command
-    def get(self):
-        print("instrument get")
+    def get(self, parser):
+        """Get instrument details"""
+        print("register instrument get")
+        parser.add_argument("name", help="Instrument name")
+        return self._get
+
+    def _get(self):
+        print("process instrument get")
 
     @cli_command
-    def details(self):
-        print("instrument details")
+    def search(self, parser):
+        """Search for instrument"""
+        print("register instrument search")
+        parser.add_argument("term", help="Search term")
+        return self._search
+
+    def _search(self):
+        print("process instrument search")
 
 
-def register_command_parsers(root_subparser):
-    for command_cls in GenericCommand.__subclasses__():
-        print(f"{command_cls}")
+class PositionsCommand(GenericCommand):
+    """Manage positions"""
+
+    cli_name = "positions"
+
+    @cli_command
+    def get(self, parser):
+        """Get all positions"""
+        print("register positions get")
+        parser.add_argument("--all", help="Get all")
+        return self._get
+
+    def _get(self):
+        print("process positions get")
+
+
+def register_command_parsers(cls, root_subparser):
+    dispatch_map = {}
+    for command_cls in cls.__subclasses__():
+        parser_cmd = root_subparser.add_parser(command_cls.cli_name,
+                                               help=command_cls.__doc__)
+        subparsers_cmd = parser_cmd.add_subparsers(
+            dest=f"{command_cls.cli_name}_command",
+            required=True
+        )
+        for action in command_cls._cli_command:
+            obj, action = command_cls.cli_name, action
+            parser_action = subparsers_cmd.add_parser(action,
+                                                      help=getattr(command_cls, action).__doc__)
+            dispatch_map[(obj, action)] = getattr(command_cls(), action)(parser_action)
+    return dispatch_map
 
 
 def build_parser():
@@ -46,34 +89,35 @@ def build_parser():
     )
     subparsers = parser.add_subparsers(dest="object", required=True)
 
-    register_command_parsers(subparsers)
+    dispatch_map = register_command_parsers(GenericCommand, subparsers)
+    print(dispatch_map)
 
-    # ** instrument **
-    parser_instrument = subparsers.add_parser("instrument", help="Instruments")
-    subparsers_instrument = parser_instrument.add_subparsers(
-        dest="instrument_command", required=True
-    )
-    # instrument -> search
-    parser_instrument_search = subparsers_instrument.add_parser(
-        "search", help="Search for markets"
-    )
-    parser_instrument_search.add_argument("term", type=str, help="Search term")
-    # instrument -> get
-    parser_instrument_get = subparsers_instrument.add_parser(
-        "get", help="Get instrument details"
-    )
-    parser_instrument_get.add_argument("name", type=str, help="Instrument name")
-
-    # ** positions **
-    parser_positions = subparsers.add_parser("positions", help="Manage open positions")
-    subparsers_positions = parser_positions.add_subparsers(
-        dest="positions_command", required=True
-    )
-    # positions -> get
-    parser_positions_get = subparsers_positions.add_parser(
-        "get", help="Get open positions"
-    )
-    parser_positions_get.add_argument("--all")
+    # # ** instrument **
+    # parser_instrument = subparsers.add_parser("instrument", help="Instruments")
+    # subparsers_instrument = parser_instrument.add_subparsers(
+    #     dest="instrument_command", required=True
+    # )
+    # # instrument -> search
+    # parser_instrument_search = subparsers_instrument.add_parser(
+    #     "search", help="Search for markets"
+    # )
+    # parser_instrument_search.add_argument("term", type=str, help="Search term")
+    # # instrument -> get
+    # parser_instrument_get = subparsers_instrument.add_parser(
+    #     "get", help="Get instrument details"
+    # )
+    # parser_instrument_get.add_argument("name", type=str, help="Instrument name")
+    #
+    # # ** positions **
+    # parser_positions = subparsers.add_parser("positions", help="Manage open positions")
+    # subparsers_positions = parser_positions.add_subparsers(
+    #     dest="positions_command", required=True
+    # )
+    # # positions -> get
+    # parser_positions_get = subparsers_positions.add_parser(
+    #     "get", help="Get open positions"
+    # )
+    # parser_positions_get.add_argument("--all")
 
     return parser
 
--- src/pygindex/utils.py
+++ src/pygindex/utils.py
@@ -15,6 +15,7 @@ def method_labeler(obj, owner, name, label):
 
 class PluggableDecorator:
     _set_name_callback = None
+    _pre_callback = None
 
     def __init__(self, fn):
         self.fn = fn
@@ -23,13 +24,19 @@ class PluggableDecorator:
         self._set_name_callback(owner, name)
 
     def __get__(self, instance, owner):
-        return functools.partial(self, instance)
+        partial_func = functools.partial(self, instance)
+        partial_func.__doc__ = self.fn.__doc__
+        return partial_func
 
     def __call__(self, *args, **kwargs):
+        if self._pre_callback:
+            self._pre_callback(*args, **kwargs)
         return self.fn(*args, **kwargs)
 
     @classmethod
-    def build_decorator_class(cls, set_name_callback):
+    def build_decorator_class(cls,
+                              set_name_callback,
+                              pre_exec_callback=None):
         cls_name = f"{cls.__name__}-{uuid.uuid4().hex[:5]}"
         cls._set_name_callback = set_name_callback
         return type(cls_name, (cls,), dict(cls.__dict__))
