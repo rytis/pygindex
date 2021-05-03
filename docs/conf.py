73b9a7d9410540a256c01f97ce7e8bcefae16089
Rytis Sileika <rts.pub@pulegium.eu>
2021-05-03 16:29:02 +0100

    fix docs build

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Rytis Sileika'
 author = 'Rytis Sileika'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.3'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
