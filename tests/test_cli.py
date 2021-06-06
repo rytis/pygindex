8262b2a24beac96c12d4aca2b7bb35850bf55ba5
Rytis Sileika <rts.pub@pulegium.eu>
2021-06-06 07:28:27 +0100

    test date parsing

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,7 @@
 import pytest
-from pygindex.cli import app
+from datetime import datetime
+from dateutil import tz
+from pygindex.cli import app, InstrumentCommand
 
 
 def test_api():
@@ -7,3 +9,17 @@ def test_api():
         app()
     assert exc.type == SystemExit
     assert exc.value.code == 2
+
+
+def test_date_parsing_datetime():
+    """Test parsing date time strings"""
+    assert InstrumentCommand._parse_date("2021/1/20") == datetime(2021, 1, 20, tzinfo=tz.tzutc())
+    assert InstrumentCommand._parse_date("2021/1/20 10:20") == datetime(2021, 1, 20, 10, 20, tzinfo=tz.tzutc())
+
+
+def test_date_parsing_relative():
+    """Test parsing relative 'human language' date time strings"""
+
+
+def test_date_parsing_now():
+    """Test we respond to 'now' keyword"""
